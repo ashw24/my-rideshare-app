e9deb433bdd3a2b36b95562aac4f17783aedc9a6
ralrocks <rahulrani03@gmail.com>
2023-11-11 15:36:30 -0800

    passenger.css

---
+++ src/components/Passenger.css
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class RideForm extends React.Component {
+  handleAskForFare = () => {
+    // Logic for asking the passenger how much they are willing to pay
+    alert('How much are you willing to pay for this ride?');
+  };
+
+  handleCancel = () => {
+    // Logic for canceling the ride
+    alert('Ride canceled.');
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Ride Request Form</h2>
+        <form>
+          {/* Add your form fields here as needed */}
+          <label>
+            How much are you willing to pay for this ride?
+            <input type="text" />
+          </label>
+          <br />
+          <button type="button" onClick={this.handleAskForFare}>
+            Ask for Fare
+          </button>
+          <button type="button" onClick={this.handleCancel}>
+            Cancel
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RideForm;
