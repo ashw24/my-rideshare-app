1c1cbbfabfdf20972b7a7755863e54065f4ad508
ashw24 <ashchembu@gmail.com>
2023-11-11 18:28:44 -0800

    hi

--- src/components/Passenger.js
+++ src/components/Passenger.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Passenger.css';
+
 class RideForm extends React.Component {
   handleAskForFare = () => {
     // Logic for asking the passenger how much they are willing to pay
@@ -16,16 +17,14 @@ class RideForm extends React.Component {
       <div>
         <h2>Ride Request Form</h2>
         <form>
-          {/* Add your form fields here as needed */}
           <label>
             How much are you willing to pay for this ride?
-            <input type="text" />
+            <input type="text" placeholder="$5.00" />
           </label>
-          <br />
           <button type="button" onClick={this.handleAskForFare}>
             Ask for Fare
           </button>
-          <button type="button" onClick={this.handleCancel}>
+          <button type="button" className="cancel-button" onClick={this.handleCancel}>
             Cancel
           </button>
         </form>
